3128940177d2c6ab9dfe9cb0b15324425e78a9d6
Facundo De Lucia <facundodeluciajudo@gmail.com>
2024-06-10 04:25:50 +0100

    fixes and added build folder as 'docs' folder

---
+++ docs/precache-manifest.7a8140e32f1a9b92c6a9b2417954ac37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c0cbca3b6b1e3121804d80d984523c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07cc2adab345e7793fd7",
+    "url": "/static/css/main.fcb8d54d.chunk.css"
+  },
+  {
+    "revision": "8f40a741bd4b090623c6",
+    "url": "/static/js/2.3a0a1c4c.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/static/js/2.3a0a1c4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07cc2adab345e7793fd7",
+    "url": "/static/js/main.6d91c164.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
